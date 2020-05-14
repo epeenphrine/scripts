7a433d76ee2cd550dd99b279d1d6bf5244cf2a82
dong <donghyunwon610@gmail.com>
2020-05-14 16:18:36 -0400

    fix

--- linux/kvm-qemu-install.sh
+++ linux/kvm-qemu-install.sh
@@ -1,5 +1,7 @@
 
 # kvm-qemu install 
+# optional check for vm compatibility
+## grep -o 'vmx\|svm' /proc/cpuinfo
 
 apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin virt-manager && 
 adduser $USER libvirt
